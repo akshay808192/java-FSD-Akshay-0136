6ba3599c37a593f839eebde5a5ec4f6890104c9b
akshay808192 <pranshusingh4138@gmail.com>
2026-09-24 12:55:38 +0530

    Update App component

--- reactvite/src/App.jsx
+++ reactvite/src/App.jsx
@@ -2,22 +2,23 @@ import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
+import mypic from './image/mypic.jpg'
 import './App.css'
-import cat from "./image/mypic.jpg";
-import UseProps from './component/UseProps'
-import ICardGallery from './component/ICardGallery'
+
 function App() {
   const a=20;
-   const name="Rahul";
-
-
-
+  const name="Akshay Pratap";
+  const roll=2400320100136;
+  
   return (
-    <div>
-     
-     {/* <UseProps /> */}
+    <div style={{border:'5px solid red',width:'300px',height:'450px'}}>
+      <h2>Welcome to React vite</h2>
+      <h3 style={{color:'brown'}}>ABES Engineering College</h3>
+      <h2 style={{color:'white'}}>Name:{name}</h2>
+      <h3>Roll:{roll}</h3>
+      
 
-     <ICardGallery />
+      <img src={mypic} height={200} width={200} alt="" />
     </div>
   )
 }
